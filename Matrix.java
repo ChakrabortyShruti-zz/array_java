9678d10c8e8321201652998051615bf4fc8c9869
Shruti Chakraborty <shruti.chakraborty16@gmail.com>
2016-02-18 19:32:07 +0530

    refactored the code for managing the encapsulation of the class-'Matrix'.

--- Matrix.java
+++ Matrix.java
@@ -1,19 +1,42 @@
 class Matrix{
-    // int matrix[][];
-    public Matrix(){
+    private int rows;
+    private int columns;
+    private int[][] matrixElements;
+
+    public Matrix(int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
+        this.matrixElements = new int[rows][columns];
     }
 
-    public int[][] createMatrix(int rows, int columns){
-        return new int[rows][columns];
+    public void initializeValue(int row,int column,int value){
+        this.matrixElements[row][column] = value;
     }
 
-    public int[][] initializeMatrix(int value, int[][] matrix){
-        // System.out.println(matrix.length);
-        for (int i=0;i<matrix.length;i++) {
-            for (int j=0;j<matrix[i].length;j++) {
-                matrix[i][j] = value++;
+    public int getMatrixValue(int row,int column){
+        return this.matrixElements[row][column];
+    }
+
+    public Matrix addWith(Matrix matrix1){
+        for (int i=0;i<this.rows;i++) {
+            for (int j=0;j<this.columns;j++) {
+                int sum = this.getMatrixValue(i,j) + matrix1.getMatrixValue(i,j);
+                this.initializeValue(i,j,sum);
             }
         }
-        return matrix;
+        return this;
+    }
+
+    public Matrix multiplyWith(Matrix matrix1){
+        for (int i=0;i<this.rows;i++) {
+            int sumOfProduct = 0;
+            for (int j=0;j<matrix1.rows;j++ ) {
+                sumOfProduct += this.getMatrixValue(i,j) * matrix1.getMatrixValue(j,i);
+            }
+            for(int k=0;k<this.rows;k++){
+                this.initializeValue(i,k,sumOfProduct);
+            }
+        }
+        return this;
     }
 }
--- TestMatrix.java
+++ TestMatrix.java
@@ -4,29 +4,66 @@ import org.junit.Before;
 
 
 public class TestMatrix{
-    Matrix m;
-    @Before
-    public void beforeEach(){
-        this.m = new Matrix();
+    @Test
+    public void initialize_matrix_should_initialize_the_matrix_with_values(){
+        Matrix matrix = new Matrix(4,3);
+
+        matrix.initializeValue(0,0,12);
+        matrix.initializeValue(0,1,1);
+        matrix.initializeValue(0,2,11);
+        matrix.initializeValue(1,0,13);
+        matrix.initializeValue(1,1,19);
+        matrix.initializeValue(1,2,23);
+        matrix.initializeValue(2,0,3);
+        matrix.initializeValue(2,1,30);
+        matrix.initializeValue(2,2,5);
+        matrix.initializeValue(3,0,7);
+        matrix.initializeValue(3,1,22);
+        matrix.initializeValue(3,2,92);
+
+        assertEquals(12,matrix.getMatrixValue(0,0));
+        assertEquals(30,matrix.getMatrixValue(2,1));
+        assertEquals(92,matrix.getMatrixValue(3,2));
+
     }
 
-    @Test
-    public void createMatrix(){
-        int matrix[][] = this.m.createMatrix(4,3);
-        assertEquals(4,matrix.length);
-        assertEquals(3,matrix[0].length);
-    }
+     @Test
+     public void add_matrices_should_add_two_matrices(){
+         Matrix firstMatrix = new Matrix(2,2);
 
-    @Test
-    public void initializeMatrix(){
-        int matrix[][] = this.m.createMatrix(4,3);
-        this.m.initializeMatrix(0,matrix);
-        assertEquals(0,matrix[0][0]);
-        assertEquals(3,matrix[1][0]);
+         firstMatrix.initializeValue(0,0,12);
+         firstMatrix.initializeValue(0,1,1);
+         firstMatrix.initializeValue(1,0,13);
+         firstMatrix.initializeValue(1,1,19);
+
+         Matrix secondMatrix = new Matrix(2,2);
+
+         secondMatrix.initializeValue(0,0,20);
+         secondMatrix.initializeValue(0,1,11);
+         secondMatrix.initializeValue(1,0,17);
+         secondMatrix.initializeValue(1,1,9);
+
+         Matrix result = firstMatrix.addWith(secondMatrix);
+
+         assertEquals(32,result.getMatrixValue(0,0));
+         assertEquals(28,result.getMatrixValue(1,1));
      }
 
      @Test
-     public void addMatrix(){
+     public void multiply_matrices_should_multiply_two_matrices(){
+         Matrix firstMatrix = new Matrix(1,2);
+
+         firstMatrix.initializeValue(0,0,1);
+         firstMatrix.initializeValue(0,1,1);
+
+         Matrix secondMatrix = new Matrix(2,1);
+
+         secondMatrix.initializeValue(0,0,1);
+         secondMatrix.initializeValue(1,0,1);
+
+         Matrix result = firstMatrix.multiplyWith(secondMatrix);
+
+         assertEquals(2,result.getMatrixValue(0,0));
 
      }
 }
